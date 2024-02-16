e8e82b53f1d05117f3b8c9dbc2c5206c0842ce21
Gerion Entrup <entrup@sra.uni-hannover.de>
2024-02-16 13:18:03 +0100

    cleanup

--- appl/compile_zephyr_app.py
+++ appl/compile_zephyr_app.py
@@ -41,18 +41,6 @@ def main():
                         help='Ninja executable.',
                         required=True,
                         type=pathlib.Path)
-    # parser.add_argument('--get-bc-program',
-    #                     help='get-bc executable.',
-    #                     required=True,
-    #                     type=pathlib.Path)
-    # parser.add_argument('--llvm-objcopy-program',
-    #                     help='llvm-objcopy executable.',
-    #                     required=True,
-    #                     type=pathlib.Path)
-    # parser.add_argument('--llvm-ld-program',
-    #                     help='lld executable.',
-    #                     required=True,
-    #                     type=pathlib.Path)
     parser.add_argument('--zephyr-dir',
                         help='Path to the Zephyr RTOS.',
                         required=True,
@@ -65,10 +53,6 @@ def main():
                         help='Bitcode output file.',
                         required=True,
                         type=pathlib.Path)
-    # parser.add_argument('--llvm-bindir',
-    #                     help='Directory that contains the LLVM tools.',
-    #                     required=True,
-    #                     type=pathlib.Path)
     parser.add_argument('--cmake-args',
                         help='CMake arguments (given as "foo=bar")',
                         required=True,
@@ -86,9 +70,6 @@ def main():
     eprint("BLALBAL", args.zephyr_dir, args.zephyr_dir.absolute())
     cmake_env = {**os.environ}
     cmake_env['ZEPHYR_BASE'] = str(args.zephyr_dir.absolute())
-    # cmake_env['GLLVM_OBJCOPY'] = str(args.llvm_objcopy_program.absolute())
-    # cmake_env['GLLVM_LD'] = str(args.llvm_ld_program.absolute())
-    # cmake_env['WLLVM_OUTPUT_LEVEL'] = 'DEBUG'
     cmake_cmd = [
         args.cmake_program,
         '-S', str(args.app_dir.absolute()), '-GNinja'
@@ -102,106 +83,7 @@ def main():
     assert image.is_file()
 
     shutil.copyfile(image, args.output.absolute())
-    # get_bc_cmd = [args.get_bc_program, '-o', args.output.absolute(), image]
-    # run('Executing get-bc', get_bc_cmd, cwd=args.cmake_builddir, env=cmake_env)
 
 
 if __name__ == '__main__':
     main()
-
-
-# from argparse import ArgumentParser
-# import os
-# import shutil
-# import importlib
-# import sys
-#
-# parser = ArgumentParser()
-# parser.add_argument('--objcopy', type=str)
-# parser.add_argument('--objdump', type=str)
-# parser.add_argument('--ranlib', type=str)
-# parser.add_argument('--ld', type=str)
-# parser.add_argument('--ar', type=str)
-# parser.add_argument('--size', type=str)
-# parser.add_argument('--nm', type=str)
-# parser.add_argument('--source_dir', type=str)
-# parser.add_argument('--build_dir', type=str)
-# parser.add_argument('--zephyr_root', type=str)
-# parser.add_argument('--board', type=str)
-# parser.add_argument('--libgcc', type=str)
-# parser.add_argument('--cc', type=str)
-# parser.add_argument('--llc', type=str)
-# parser.add_argument('--llvm_link', type=str)
-# parser.add_argument('name', metavar='N', type=str)
-#
-# args = parser.parse_args()
-#
-# from util import KConfigFile
-#
-# # Clear the build dir if it exits and we can't prove that the existing build is for the same board.
-# # This is required by the zephyr build system. Otherwise cmake runs into caching issues.
-# # TODO: Investigate how ninja clean handles zephyr
-# same_board = False
-# try:
-#     if os.path.exists(args.build_dir):
-#         config = KConfigFile(os.path.join(args.build_dir, 'zephyr/.config'))
-#         same_board = config['CONFIG_BOARD'] == args.board
-#         if not same_board:
-#             print('Board might have changed, regenerating cmake...')
-#             shutil.rmtree(args.build_dir)
-#         else:
-#             print('Board has not changed, skipping cmake...')
-#
-# except FileNotFoundError:
-#     print('Could not detect board, regenerating...')
-#     shutil.rmtree(args.build_dir)
-# try:
-#     os.mkdir(args.build_dir)
-# except FileExistsError:
-#     pass
-#
-# # NOTE: Whenever any of these flags change, regenerate manually
-# if not same_board:
-#     cmake_call = 'cmake -G Ninja'
-#     cmake_call += ' -D BOARD=' + args.board
-#     cmake_call += ' -D ZEPHYR_TOOLCHAIN_VARIANT=llvm'
-#     cmake_call += ' -D ZEPHYR_BASE=' + args.zephyr_root
-#     cmake_call += ' -D TOOLCHAIN_ROOT=' + args.zephyr_root
-#     cmake_call += ' -D CMAKE_C_COMPILER=' + args.cc
-#     cmake_call += ' -D CMAKE_CXX_COMPILER=' + args.cc
-#     cmake_call += ' -D CMAKE_LLC=' + args.llc
-#     cmake_call += ' -D CMAKE_LLVM_LINK=' + args.llvm_link
-#     # Normally cmake *should* find them by itself, but this seems to be the only way to make it work
-#     # reliably
-#     cmake_call += ' -D CMAKE_OBJCOPY=' + args.objcopy
-#     cmake_call += ' -D CMAKE_OBJDUMP=' + args.objdump
-#     cmake_call += ' -D CMAKE_NM=' + args.nm
-#     cmake_call += ' -D CMAKE_AR=' + args.ar
-#     # Override the linker that clang uses for the exe/elf. Clang defaults to lld which is incompatible
-#     # with the generated linker scripts. Note that setting CMAKE_LINKER will NOT work since clang
-#     # ignores that.
-#     cmake_call += ' -D CMAKE_EXE_LINKER_FLAGS="-fuse-ld=' + args.ld + ' -L ' + args.libgcc + '"' #+ ' -v"'
-#     # Disable common warnings that gcc does not emit but clang does. Typedef redefinition will be fixed
-#     # in https://github.com/zephyrproject-rtos/zephyr/pull/29359
-#     cmake_call += ' -D EXTRA_CFLAGS="-Wno-typedef-redefinition -Wno-unused-command-line-argument"'
-#     cmake_call += ' -S ' + args.source_dir
-#     cmake_call += ' -B ' + args.build_dir
-#
-#     # Generate cmake files
-#     # Fail with an exeception so that meson/ninja knows something went wrong.
-#     ret = os.system(cmake_call)
-#     if ret != 0:
-#         raise RuntimeError("Cmake failed!")
-#
-# # Invoke ninja to build
-# ret = os.system('ninja -C ' + args.build_dir)
-# if ret != 0:
-#     raise RuntimeError("Ninja failed!")
-# # Copy the lib.ll and .config to the top level for ease of use
-# try:
-#     shutil.copyfile(os.path.join(args.build_dir, 'app/libapp.ll'), os.path.join(args.build_dir,
-#         '..', args.name + '.ll'))
-#     shutil.copyfile(os.path.join(args.build_dir, 'zephyr/.config'), os.path.join(args.build_dir,
-#         '..', args.name + '.config'))
-# except FileNotFoundError:
-#     pass
